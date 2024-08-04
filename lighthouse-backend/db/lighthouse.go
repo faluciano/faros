c60fa3e68a2df281a4fdeff95e128d02baaff203
Felix Luciano Salomon <felixluciano.a@gmail.com>
2024-08-04 15:54:19 -0700

    Added backend functions to retrieve lighthouses by country

--- lighthouse-backend/db/lighthouse.go
+++ lighthouse-backend/db/lighthouse.go
@@ -36,3 +36,57 @@ func GetLighthouses() ([]schemas.Lighthouse, error) {
 
 	return lighthouses, nil
 }
+
+func GetLighthousesByCountry(country string) ([]schemas.Lighthouse, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return nil, err
+	}
+
+	SUPABASE_URL := os.Getenv("SUPABASE_URL")
+	SUPABASE_KEY := os.Getenv("SUPABASE_KEY")
+	supabase := supa.CreateClient(SUPABASE_URL, SUPABASE_KEY)
+
+	if supabase == nil {
+		log.Fatal("Failed to create Supabase client")
+		return nil, err
+	}
+	lighthouses := []schemas.Lighthouse{}
+	query := supabase.DB.From("lighthouses").Select("*").Eq("country", country)
+	err = query.Execute(&lighthouses)
+
+	if err != nil {
+		log.Fatal("Request failed:", err)
+		return nil, err
+	}
+
+	return lighthouses, nil
+}
+
+func GetLighthousesByState(state string) ([]schemas.Lighthouse, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return nil, err
+	}
+
+	SUPABASE_URL := os.Getenv("SUPABASE_URL")
+	SUPABASE_KEY := os.Getenv("SUPABASE_KEY")
+	supabase := supa.CreateClient(SUPABASE_URL, SUPABASE_KEY)
+
+	if supabase == nil {
+		log.Fatal("Failed to create Supabase client")
+		return nil, err
+	}
+	lighthouses := []schemas.Lighthouse{}
+	query := supabase.DB.From("lighthouses").Select("*").Eq("state", state)
+	err = query.Execute(&lighthouses)
+
+	if err != nil {
+		log.Fatal("Request failed:", err)
+		return nil, err
+	}
+
+	return lighthouses, nil
+}
