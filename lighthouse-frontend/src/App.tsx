b13f31e7b2aced56e8ea554712d092c2644f330f
Felix Luciano Salomon <felixluciano.a@gmail.com>
2024-08-04 16:22:37 -0700

    Added dummy house for when backend is not connected

--- lighthouse-frontend/src/App.tsx
+++ lighthouse-frontend/src/App.tsx
@@ -2,13 +2,36 @@ import Navbar from "./components/navbar";
 import LighthouseMap from "./components/lighthousemap";
 import { useEffect, useState } from "react";
 
+interface Lighthouse {
+  id: string;
+  latitude: number;
+  longitude: number;
+  name: string;
+  image: string;
+  state: string;
+  country: string;
+}
+
+const dummyLighthouses = [
+  {
+    id: "1",
+    latitude: 47.7511,
+    longitude: -120.7401,
+    name: "Dummy Lighthouse",
+    image: "https://example.com/lighthouse.jpg",
+    state: "Washington",
+    country: "United States",
+  },
+];
+
 function App() {
-  const [lighthouses, setLighthouses] = useState([]);
+  const [lighthouses, setLighthouses] = useState<Lighthouse[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/api/lighthouses")
       .then((response) => response.json())
-      .then((data) => setLighthouses(data));
+      .then((data) => setLighthouses(data))
+      .catch(() => setLighthouses(dummyLighthouses));
   }, []);
 
   return (
