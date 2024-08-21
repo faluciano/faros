8a313e100ef916c285a0da98f5e0d6afb5438424
Felix Luciano Salomon <felixluciano.a@gmail.com>
2024-08-20 22:06:44 -0700

    Updated user schema

--- lighthouse-backend/schemas/user.go
+++ lighthouse-backend/schemas/user.go
@@ -4,4 +4,7 @@ type User struct {
 	ID                 string   `json:"id"`
 	Username           string   `json:"username"`
 	VisitedLightHouses []string `json:"visitedLightHouses"`
+	StampedLightHouses []string `json:"stampedLightHouses"`
+	Friends            []string `json:"friends"`
+	SavedLightHouses   []string `json:"savedLightHouses"`
 }
